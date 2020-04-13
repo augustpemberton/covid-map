adf2a6a723c02c73dc7f7638a0b8a15fe8b863bd
august <augustmpemberton@gmail.com>
2020-04-13 10:02:37 +0100

    First commit

---
+++ src/components/Map.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    test
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Map',
+  data() {
+    return {
+      json: {}
+    }
+  },
+  mounted() {
+    this.json = this.$papa.parse('/NHSR_Cases_table.csv', {
+      download: true
+    })
+    console.log(this.json);
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VuePapaParse from 'vue-papa-parse'
+
+Vue.use(VuePapaParse)
 
 Vue.config.productionTip = false
 
